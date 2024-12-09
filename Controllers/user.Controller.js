47242fc66316902a287d8cdfeed504c67a64d54d
Yosf96633 <yosf96633@gmail.com>
2024-12-09 17:24:18 +0500

    ss

--- Controllers/user.Controller.js
+++ Controllers/user.Controller.js
@@ -37,8 +37,8 @@ export const Login = async (req, res) => {
       maxAge: 1000 * 60 * 60 * 24 * 7,
       path: "/",
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      secure: false,
+      sameSite:"None",
+      secure: true,
 
     });
     return res.status(200).json({
